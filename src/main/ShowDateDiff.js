cea4a141099d62d5c375321acdfcbf5d1eb7747f
reproducepark <reproducepark@kaist.ac.kr>
2024-08-07 19:21:54 +0900

    datediff minute error resolved

--- src/main/ShowDateDiff.js
+++ src/main/ShowDateDiff.js
@@ -34,12 +34,11 @@ const DateDiff2 = styled.div`
 
 const ShowDateDiff = ({ lockedDate }) => {
     const [timeDifference, setTimeDifference] = useState(0);
-    const [overNine, setOverNine] = useState(true);
 
     const formatTimeDifference = (difference) => {
         const days = Math.floor(difference / (1000 * 60 * 60 * 24));
         const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((difference % (1000 * 60)) / (1000 * 60));
+        const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
         const seconds = Math.floor((difference % (1000 * 60)) / 1000);
 
         return [`${days}일`, `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`];
@@ -52,13 +51,6 @@ const ShowDateDiff = ({ lockedDate }) => {
             setTimeDifference(difference);
         }, 1000);
 
-        // when time difference is bigger than 9 days, change marginLeft
-        if (timeDifference > 9 * 24 * 60 * 60 * 1000) {
-            setOverNine(true);
-        } else {
-            setOverNine(false);
-        }
-
         return () => {
             clearInterval(timerID);
         };
@@ -66,7 +58,7 @@ const ShowDateDiff = ({ lockedDate }) => {
 
     return (
         <Container>
-            <DateDiff1 overNine={overNine}>{formatTimeDifference(timeDifference)[0]}</DateDiff1>
+            <DateDiff1>{formatTimeDifference(timeDifference)[0]}</DateDiff1>
             <DateDiff2>{formatTimeDifference(timeDifference)[1]}</DateDiff2>
         </Container>
     );
